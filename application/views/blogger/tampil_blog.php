795531c5d1762c20a075ebb1d74d02fbcec0370f
WINDOWS\WINDOWS 10 <yuliartikanovelia@gmail.com>
2018-05-14 22:07:18 +0700

    Tugas 07 - 21

--- application/views/blogger/tampil_blog.php
+++ application/views/blogger/tampil_blog.php
@@ -1,15 +1,28 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<main role="main">
+
+<section class="jumbotron text-center">
 <div class="container">
-	<table class="table table-hover">
-		<h2>Post Article</h2>
-	<tr>
-		<td><b>ID</b></td>
-		<td><b>Judul</b></td>
-		<td><b>Isi</b></td>
-		<td><b>Tanggal</b></td>
-		<td><b>Kategori</b></td>
-		<td><b>Gambar</b></td>
-		<td colspan="3"><b>Action</b></td>
-	</tr>
+		<h1 class="jumbotron-heading">Post Article</h1>
+	</div>
+</section>
+    
+    <section class="py-5 bg-light">
+        <div class="container">
+            <div class="row">
+                <table id="dt-basic" class="table table-striped table-bordered">
+		<thead>
+			<tr>
+				<td><b>ID</b></td>
+				<td><b>Judul</b></td>
+				<td><b>Isi</b></td>
+				<td><b>Tanggal</b></td>
+				<td><b>Kategori</b></td>
+				<td><b>Gambar</b></td>
+				<td colspan="3"><b>Action</b></td>
+			</tr>
+		</thead>
+<tbody>
 <?php
 foreach ($artikel->result_array() as $row)
 		{
@@ -26,7 +39,23 @@ foreach ($artikel->result_array() as $row)
         	echo "</tr>";
 
 		}
-		echo "<td><a href='".site_url('blog')."'>Back</a></td>";
+		echo "<td><a href='".site_url('blog')."'>Back</a></td>"; 
 ?>
+</tbody>
 </table>
 </div>
+</div>
+</section>
+
+<main>
+<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/jquery.dataTables.min.css">
+<script src="<?php echo base_url() ?>assets/js/jquery.dataTables.min.js"></script>
+<script src="<?php echo base_url() ?>assets/js/jquery.dataTables.bootstrap4.min.js"></script>
+<script>
+    jQuery(document).ready(function(){
+
+        // Contoh inisialisasi Datatable tanpa konfigurasi apapun
+        // #dt-basic adalah id html dari tabel yang diinisialisasi
+        $('#dt-basic').DataTable();
+    });
+</script>
